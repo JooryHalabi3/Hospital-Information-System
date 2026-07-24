da0c06e4c67b94f3cde508fa342cf98267d26e26
Joory Halabi <Joory6067@hotmail.com>
2026-07-24 10:10:12 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql2");
-require("dotenv").config(); // تحميل المتغيرات من .env
-
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 4000,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "isrgrootx1.pem")),
+    minVersion: "TLSv1.2",
+  },
 });
 
 db.connect((err) => {
